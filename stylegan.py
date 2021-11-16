9918807ffb2ac991b7bd66df913ea81b91abf45b
jattardo1 <60160014+jattardo1@users.noreply.github.com>
2021-11-16 17:03:27 -0500

    Update and rename stylegan3.py to stylegan.py

--- stylegan.py
+++ stylegan.py
@@ -101,24 +101,13 @@ class MyConv2d(nn.Module):
 
 
 class NoiseLayer(nn.Module):
-    """adds noise. noise is per pixel (constant over channels) with per-channel weight"""
-
-    def __init__(self, channels):
+    def __init__(self, channel):
         super().__init__()
-        self.weight = nn.Parameter(torch.zeros(channels))
-        self.noise = None
 
-    def forward(self, x, noise=None):
-        if noise is None and self.noise is None:
-            noise = torch.randn(x.size(0), 1, x.size(
-                2), x.size(3), device=x.device, dtype=x.dtype)
-        elif noise is None:
-            # here is a little trick: if you get all the noiselayers and set each
-            # modules .noise attribute, you can have pre-defined noise.
-            # Very useful for analysis
-            noise = self.noise
-        x = x + self.weight.view(1, -1, 1, 1) * noise
-        return x
+        self.weight = nn.Parameter(torch.zeros(1, channel, 1, 1))
+
+    def forward(self, image, noise):
+        return image + self.weight * noise
 
 
 class StyleMod(nn.Module):
