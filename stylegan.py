d873f4fc4f60944736f0e63365094953f000c1dd
jattardo1 <60160014+jattardo1@users.noreply.github.com>
2021-11-17 18:22:21 -0500

    Update stylegan.py

--- stylegan.py
+++ stylegan.py
@@ -80,7 +80,7 @@ class MyConv2d(nn.Module):
             w = w[:, :, 1:, 1:] + w[:, :, :-1, 1:] + \
                 w[:, :, 1:, :-1] + w[:, :, :-1, :-1]
             x = F.conv_transpose2d(
-                x, w, stride=1, padding=int((w.size(-1)-1)//2))
+                x, w, stride=2, padding=int((w.size(-1)-1)//2))
             have_convolution = True
         elif self.upscale is not None:
             x = self.upscale(x)
