d0e70b8fef8eb61a8aa3ddc019aab471f673c02a
jattardo1 <60160014+jattardo1@users.noreply.github.com>
2021-11-17 21:10:56 -0500

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,10 +1,7 @@
-# Python program for face
-# comparison
-
+#UPLOAD THE IMAGES YOU WANT TO USE TO THE INTERNET TO GIVE THEM A LINK THEN PASTE THAT LINK IN THE CORRECT LOCATION IN THE CODE
 
 from __future__ import print_function, unicode_literals
 from facepplib import FacePP, exceptions
-import emoji
 
 
 # define global variables
@@ -20,16 +17,11 @@ beauty_score_and_emotion_recognition = ""
 def face_comparing(app, Image1, Image2, im_name):
 	
 	print()
-	print('-'*30)
 	print('Comparing Photographs......')
-	print('-'*30)
 
 
 	cmp_ = app.compare.get(image_url1 = Image1,
 						image_url2 = Image2)
-
-	print('Photo1', '=', cmp_.image1)
-	print('Photo2', '=', cmp_.image2)
 
 	print("The "+im_name +" implimentation has accuracy of: " +str(cmp_.confidence)+ "%")
 
@@ -57,14 +49,14 @@ if __name__ == '__main__':
 		]
 		
 		# Pair 1
-		image1 = 'https://i.postimg.cc/vD51K85H/Screen-Shot-2021-11-17-at-7-58-16-PM.png'
+		image1 = 'https://i.postimg.cc/vD51K85H/Screen-Shot-2021-11-17-at-7-58-16-PM.png' 
 		image2 = 'https://i.postimg.cc/8jDFg8bd/Screen-Shot-2021-11-17-at-7-58-39-PM.png'
-		face_comparing(app_, image1, image2, "New")
+		face_comparing(app_, image1, image2, "Old")
 		
 		# Pair2
 		image1 = 'https://i.postimg.cc/8jDFg8bd/Screen-Shot-2021-11-17-at-7-58-39-PM.png'
 		image2 = 'https://i.postimg.cc/GHQ4cMMW/Screen-Shot-2021-11-17-at-7-58-28-PM.png'
-		face_comparing(app_, image1, image2, "Original")		
+		face_comparing(app_, image1, image2, "New")		
 
 	except exceptions.BaseFacePPError as e:
 		print('Error:', e)
